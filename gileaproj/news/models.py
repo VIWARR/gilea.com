aec3ed4dd238fef840838d838440bf8b2e7bf7fc
VIWARR <dm.pavlyuchenko@gmail.com>
2023-05-08 23:23:13 +0300

    model post

--- gileaproj/news/models.py
+++ gileaproj/news/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from gileaproj.settings import TIME_ZONE
+
+
+class Post(models.Model):
+    """CREATING A POST MODEL"""
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=5000, blank=True, null=True)
+    date_created = models.DateField(auto_now_add=True, default=TIME_ZONE.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'create post'
+        verbose_name_plural = 'create posts'
+
+    def __str__(self):
+        return self.title
