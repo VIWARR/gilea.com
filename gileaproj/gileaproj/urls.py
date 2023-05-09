1bd7d8d21694a8866f449503e364d102aed5cef4
VIWARR <dm.pavlyuchenko@gmail.com>
2023-05-09 12:33:51 +0300

    Update urls (CKEditor)

--- gileaproj/gileaproj/urls.py
+++ gileaproj/gileaproj/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
