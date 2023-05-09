787c3bf791a4a8fa864a358435228e4c669382e9
VIWARR <dm.pavlyuchenko@gmail.com>
2023-05-09 11:25:19 +0300

    Update models.py

--- gileaproj/news/models.py
+++ gileaproj/news/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
 from django.contrib.auth.models import User
-from gileaproj.settings import TIME_ZONE
 
 
 class Post(models.Model):
     """CREATING A POST MODEL"""
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=5000, blank=True, null=True)
-    date_created = models.DateField(auto_now_add=True, default=TIME_ZONE.now)
+    date_created = models.DateField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'create post'
-        verbose_name_plural = 'create posts'
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
 
     def __str__(self):
         return self.title
